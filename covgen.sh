47aad418c8c647f90f5f63a66ab1ed55585b38ff
mah0x211 <mah0x211@gmail.com>
2026-06-17 10:21:06 +0900

    Refactor tests to the testcase framework
    Move the test bodies into named testcase functions and simplify the
    coverage script and build dependencies accordingly.

--- covgen.sh
+++ covgen.sh
@@ -3,7 +3,5 @@
 set -ex
 
 mkdir -p ./coverage
-lcov -c -d ./src -o coverage/lcov.info.all
-lcov -r coverage/lcov.info.all '*/include/*' -o coverage/lcov.info.all
-lcov -r coverage/lcov.info.all '*/deps/*' -o coverage/lcov.info
+lcov -c -d ./src -o coverage/lcov.info
 # genhtml -o coverage/html coverage/lcov.info
--- test/isfile_test.lua
+++ test/isfile_test.lua
@@ -1,22 +1,28 @@
+local testcase = require('testcase')
 local assert = require('assert')
 local isfile = require('io.isfile')
 
-local f = assert(io.tmpfile())
-local mt = getmetatable(f)
-
--- test that returns true
-assert.is_true(isfile(f))
-
--- test that returns false
-local fake = setmetatable({}, mt)
-for _, v in ipairs({
-    fake,
-    'foo',
-    1,
-    {},
-    function()
-    end,
-}) do
-    assert.is_false(isfile(v))
+function testcase.isfile()
+    -- test that returns true
+    local f = assert(io.tmpfile())
+    assert.is_true(isfile(f))
 end
-setmetatable(fake, nil)
+
+function testcase.not_isfile()
+    local f = assert(io.tmpfile())
+    local mt = getmetatable(f)
+
+    -- test that returns false
+    local fake = setmetatable({}, mt)
+    for _, v in ipairs({
+        fake,
+        'foo',
+        1,
+        {},
+        function()
+        end,
+    }) do
+        assert.is_false(isfile(v))
+    end
+    setmetatable(fake, nil)
+end
